21de48a0a8d525429785ff437c174ef251286c5d
Levi Blaney <shadycuz@gmail.com>
2020-06-11 11:25:49 -0400

    write tests for new idempotent apply

--- modules/terraform/apply_test.go
+++ modules/terraform/apply_test.go
@@ -2,7 +2,6 @@ package terraform
 
 import (
 	"testing"
-	"time"
 
 	"github.com/gruntwork-io/terratest/modules/files"
 	"github.com/stretchr/testify/assert"
@@ -121,7 +120,7 @@ func TestTgApplyOutput(t *testing.T) {
 	assert.Equal(t, allOutputs, map[string]interface{}{"str": "str", "list": []interface{}{"a", "b", "c"}, "map": map[string]interface{}{"foo": "bar"}})
 }
 
-func TestIdempotentNoChanges(t *testing.T) {
+func TestIdempotentNochanges(t *testing.T) {
 	t.Parallel()
 
 	testFolder, err := files.CopyTerraformFolderToTemp("../../test/fixtures/terraform-no-error", t.Name())
@@ -132,10 +131,10 @@ func TestIdempotentNoChanges(t *testing.T) {
 		NoColor:      true,
 	}
 
-	InitAndApplyAndIdempotentE(t, options)
+	ApplyAndIdempotent(t, options)
 }
 
-func TestIdempotentWithChanges(t *testing.T) {
+func TestIdempotentWithchanges(t *testing.T) {
 	t.Parallel()
 
 	testFolder, err := files.CopyTerraformFolderToTemp("../../test/fixtures/terraform-not-idempotent", t.Name())
@@ -146,39 +145,6 @@ func TestIdempotentWithChanges(t *testing.T) {
 		NoColor:      true,
 	}
 
-	out, err := InitAndApplyAndIdempotentE(t, options)
+	require.Panicsf(t, func() { ApplyAndIdempotent(t, options) }, "Should Fail")
 
-	require.NotEmpty(t, out)
-	require.Error(t, err)
-	require.EqualError(t, err, "terraform configuration not idempotent")
-}
-
-func TestParallelism(t *testing.T) {
-	t.Parallel()
-
-	testFolder, err := files.CopyTerraformFolderToTemp("../../test/fixtures/terraform-parallelism", t.Name())
-	require.NoError(t, err)
-
-	options := &Options{
-		TerraformDir: testFolder,
-		NoColor:      true,
-	}
-
-	Init(t, options)
-
-	// Run the first time with parallelism set to 5 and it should take about 5 seconds (plus or minus 10 seconds to
-	// account for other CPU hogging stuff)
-	options.Parallelism = 5
-	start := time.Now()
-	Apply(t, options)
-	end := time.Now()
-	require.WithinDuration(t, end, start, 15*time.Second)
-
-	// Run the second time with parallelism set to 1 and it should take at least 25 seconds
-	options.Parallelism = 1
-	start = time.Now()
-	Apply(t, options)
-	end = time.Now()
-	duration := end.Sub(start)
-	require.Greater(t, int64(duration.Seconds()), int64(25))
 }
