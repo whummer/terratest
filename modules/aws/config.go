4e78647bdf4441e5053ad15cc0d7a56541b2a93c
Fernando Fernandez <ffernandezcast@outlook.com>
2020-09-15 14:44:05 +0200

    Remove sync once and avoid custom endpoint array override incluiding warning message about it

--- modules/aws/config.go
+++ modules/aws/config.go
@@ -1,14 +1,30 @@
 package aws
 
 import (
-	"sync"
+	"log"
+	"os"
 )
 
-var once sync.Once
-var GlobalCustomEndpoints *map[string]string
+var customEndpoints map[string]string
 
-func SetAwsEndpointsOverrides(endpoints *map[string]string) {
-	once.Do(func() {
-		GlobalCustomEndpoints = endpoints
-	})
+// SetAwsEndpointsOverrides function to override the aws endpoints globall one time
+func SetAwsEndpointsOverrides(endpoints map[string]string) {
+
+	if HasAwsCustomEndPoints() {
+		log.Println("The aws custom endpoints have been already setup, check your configuration. The global endpoint override is just allow one time")
+		os.Exit(1)
+	}
+	customEndpoints = endpoints
+}
+
+// HasAwsCustomEndPoints Function to verify if already has been setup the custom endpoints
+func HasAwsCustomEndPoints() bool {
+	return len(customEndpoints) > 0
+}
+
+// EndpointShouldBeOverride fucntion check if the current service evaulate has custom endpoint to be override
+func EndpointShouldBeOverride(service string) (string, bool) {
+	customServiceEndpoint, endpointShouldBeOverride := customEndpoints[service]
+
+	return customServiceEndpoint, endpointShouldBeOverride
 }
