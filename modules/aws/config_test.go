f17e0b7302a895822ef2f1231d6873c4ce4b384c
Fernando Fernandez <ffernandezcast@outlook.com>
2021-01-28 19:35:03 +0100

    Simplify assert change boolean by contains

--- modules/aws/config_test.go
+++ modules/aws/config_test.go
@@ -4,37 +4,37 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var localEndpoints = map[string]string{
+	"apigateway":     "http://localhost:4566",
+	"apigatewayv2":   "http://localhost:4566",
+	"cloudformation": "http://localhost:4566",
+	"cloudwatch":     "http://localhost:4566",
+	"dynamodb":       "http://localhost:4566",
+	"es":             "http://localhost:4566",
+	"firehose":       "http://localhost:4566",
+	"iam":            "http://localhost:4566",
+	"kinesis":        "http://localhost:4566",
+	"lambda":         "http://localhost:4566",
+	"route53":        "http://localhost:4566",
+	"redshift":       "http://localhost:4566",
+	"s3":             "http://localhost:4566",
+	"secretsmanager": "http://localhost:4566",
+	"ses":            "http://localhost:4566",
+	"sns":            "http://localhost:4566",
+	"sqs":            "http://localhost:4566",
+	"ssm":            "http://localhost:4566",
+	"stepfunctions":  "http://localhost:4566",
+	"sts":            "http://localhost:4566",
+	"ec2":            "http://localhost:4566",
+}
+
 func TestSetAwsEndpointsOverridesShouldNotAllowSetCustomoEndpointsMoreThanOnce(t *testing.T) {
 	t.Parallel()
-	var localEndpoints = map[string]string{
-		"apigateway":     "http://localhost:4566",
-		"apigatewayv2":   "http://localhost:4566",
-		"cloudformation": "http://localhost:4566",
-		"cloudwatch":     "http://localhost:4566",
-		"dynamodb":       "http://localhost:4566",
-		"es":             "http://localhost:4566",
-		"firehose":       "http://localhost:4566",
-		"iam":            "http://localhost:4566",
-		"kinesis":        "http://localhost:4566",
-		"lambda":         "http://localhost:4566",
-		"route53":        "http://localhost:4566",
-		"redshift":       "http://localhost:4566",
-		"s3":             "http://localhost:4566",
-		"secretsmanager": "http://localhost:4566",
-		"ses":            "http://localhost:4566",
-		"sns":            "http://localhost:4566",
-		"sqs":            "http://localhost:4566",
-		"ssm":            "http://localhost:4566",
-		"stepfunctions":  "http://localhost:4566",
-		"sts":            "http://localhost:4566",
-		"ec2":            "http://localhost:4566",
-	}
 
 	// Run the crashing code when DUPLICATE_ENDPOINTS is set
 	if os.Getenv("DUPLICATE_ENDPOINTS") == "1" {
@@ -59,6 +59,5 @@ func TestSetAwsEndpointsOverridesShouldNotAllowSetCustomoEndpointsMoreThanOnce(t
 	messageExpected := "The aws custom endpoints have been already setup, check your configuration. The global endpoint override is just allow one time"
 	messageReceived := got[:len(got)-1]
 
-	assert.True(t, strings.HasSuffix(messageReceived, messageExpected), messageReceived)
-
+	assert.Contains(t, messageReceived, messageExpected)
 }
