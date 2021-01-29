1d4a260a81b4bc5e11fa7d61676f128fcd90e8a6
Fernando Fernandez <ffernandezcast@gmail.com>
2021-01-29 11:23:28 +0100

    Rename test name and refactor

--- modules/aws/auth_test.go
+++ modules/aws/auth_test.go
@@ -7,32 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewAuthenticatedSessionFromDefaultCredentials(t *testing.T) {
-	localEndpoints := map[string]string{
-		"apigateway":     "http://localhost:4566",
-		"apigatewayv2":   "http://localhost:4566",
-		"cloudformation": "http://localhost:4566",
-		"cloudwatch":     "http://localhost:4566",
-		"dynamodb":       "http://localhost:4566",
-		"es":             "http://localhost:4566",
-		"firehose":       "http://localhost:4566",
-		"iam":            "http://localhost:4566",
-		"kinesis":        "http://localhost:4566",
-		"lambda":         "http://localhost:4566",
-		"route53":        "http://localhost:4566",
-		"redshift":       "http://localhost:4566",
-		"s3":             "http://localhost:4566",
-		"secretsmanager": "http://localhost:4566",
-		"ses":            "http://localhost:4566",
-		"sns":            "http://localhost:4566",
-		"sqs":            "http://localhost:4566",
-		"ssm":            "http://localhost:4566",
-		"stepfunctions":  "http://localhost:4566",
-		"sts":            "http://localhost:4566",
-		"ec2":            "http://localhost:4566",
-	}
-
-	signingRegionExpected := "custom-signing-region"
+func TestNewAuthenticatedSessionFromDefaultCredentialsShouldReturnCustomEnpointURLForS3Service(t *testing.T) {
+	const signingRegionExpected = "custom-signing-region"
 	region := GetRandomStableRegion(t, nil, nil)
 
 	SetAwsEndpointsOverrides(localEndpoints)
